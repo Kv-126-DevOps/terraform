579a1ae6dd29799b7ca636dc0ec368cfe807752a
Anatolii Pedchenko <apedc@softserveinc.com>
2022-05-23 21:19:13 +0300

    added rds module

--- sandbox/euc101/main.tf
+++ sandbox/euc101/main.tf
@@ -58,3 +58,37 @@ module "ec2-instance-service" {
     local.common_tags
   )
 }
+
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 4.3.0"
+  identifier = "postgres-default"
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14" # DB parameter group
+  major_engine_version = "14"         # DB option group
+  instance_class       = "db.t4g.micro"
+
+  allocated_storage = 10
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  db_name  = "postgres"
+  username = "postgres"
+  port     = 5432
+
+  #db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [var.subnet_id[local.env_name]]
+
+  maintenance_window      = "Mon:00:00-Mon:03:00"
+  backup_window           = "03:00-06:00"
+  backup_retention_period = 0
+
+  tags = local.common_tags
+}
