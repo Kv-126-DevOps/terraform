1788648d8b3231983371b691d78890712ed1468e
AlexPokatilov <97542627+AlexPokatilov@users.noreply.github.com>
2022-06-19 20:08:40 +0300

    1-terraform-add-infrastructure-objects-to-the-euc101sandbox-environment (#5)

--- sandbox/euc101/main.tf
+++ sandbox/euc101/main.tf
@@ -1,13 +1,9 @@
-################
-# AWS Provider #
-################
+############## AWS Provider ############
 provider "aws" {
   region = var.region
 }
 
-########################
-# Configure S3 backend #
-########################
+########## Configure S3 backend #########
 terraform {
   backend "s3" {
     bucket         = "euc101-sandbox-terraform-state"
@@ -17,9 +13,7 @@ terraform {
   }
 }
 
-##############################################################
-# Common composed values shared across the different modules #
-##############################################################
+############ Common composed values shared across the different modules ############
 locals {
   env_name = terraform.workspace
   common_tags = {
@@ -30,12 +24,159 @@ locals {
   }
 }
 
-module "rabbitmq_security_group" {
+########## Used modules #####
+
+####### Security group for RabbitMQ #########
+module "rabbitmq-security-group" {
   source              = "terraform-aws-modules/security-group/aws//modules/rabbitmq"
   version             = "~> 4.0"
   create              = var.rabbitmq_create[local.env_name]
   vpc_id              = var.vpc_id[local.env_name]
   name                = "${local.env_name}-${var.env_class}-rabbitmq-security-group"
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  tags                = local.common_tags
+  ingress_with_source_security_group_id = [
+    {
+      description              = "HTTPS for common sg"
+      rule                     = "https-443-tcp"
+      source_security_group_id = "sg-00aebda5b39acaef6"
+    },
+  ]
+  tags = local.common_tags
+}
+
+########## RabbitMQ ###########
+resource "aws_mq_broker" "rabbit" {
+  count              = var.rabbitmq_create[local.env_name] ? 1 : 0
+  broker_name        = "rabbit-${local.env_name}-${var.env_class}"
+  engine_type        = "RabbitMQ"
+  engine_version     = "3.9.16"
+  host_instance_type = "mq.t3.micro"
+  security_groups    = [module.rabbitmq-security-group.security_group_id]
+  user {
+    username = var.mquser
+    password = var.mqpass
+  }
+}
+
+
+########## Security group for RDS / RDS ##########
+module "security-group-rds" {
+  source      = "terraform-aws-modules/security-group/aws"
+  create      = var.rds_create[local.env_name]
+  name        = "${local.env_name}-${var.env_class}-rds-security-group"
+  description = "PostgreSQL with opened 5432 port within VPC"
+  vpc_id      = var.vpc_id[local.env_name]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access within VPC"
+      cidr_blocks = "172.31.0.0/16"
+    },
+  ]
+  tags = local.common_tags
+}
+
+# ########## RDS ##########
+module "aws-rds" {
+  source                    = "terraform-aws-modules/rds/aws"
+  version                   = "~> 4.3.0"
+  create                    = var.rds_create[local.env_name]
+  identifier                = "postgres-${local.env_name}-${var.env_class}"
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14" # DB parameter group
+  major_engine_version = "14"         # DB option group
+  instance_class       = "db.t4g.micro"
+  allocated_storage    = 10
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  db_name  = "postgres"
+  username = var.dbuser
+  port     = 5432
+  password = var.dbpass
+
+  # db_subnet_group_name   = var.subnet_id[local.env_name]
+  vpc_security_group_ids          = [module.security-group-rds.security_group_id]
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  backup_retention_period         = 0
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+  tags                            = local.common_tags
+}
+
+########### EC2 instances for services ##########
+module "ec2-instance-service" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 3.0"
+  create                 = var.ec2_instances_create[local.env_name]
+  for_each               = toset(["rabbit_to_db", "rest_api", "frontend", "rabbit_to_slack"])
+  name                   = "${each.key}_${local.env_name}_${var.env_class}.${var.route_53_private_zone_name[local.env_name]}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = "deploy"
+  monitoring             = true
+  vpc_security_group_ids = var.security_group_ids
+  subnet_id              = var.subnet_id[local.env_name]
+  tags = merge(
+    {
+      group = "${each.key}"
+    },
+    local.common_tags
+  )
+}
+
+######### secirity group for json-filter ###########
+module "security-group-json" {
+  source      = "terraform-aws-modules/security-group/aws"
+  create      = var.ec2_instances_create[local.env_name]
+  name        = "${local.env_name}-${var.env_class}-json_filter-security-group"
+  description = "Open 5000 port for webhooks"
+  vpc_id      = var.vpc_id[local.env_name]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5000
+      to_port     = 5000
+      protocol    = "tcp"
+      description = "Open 5000 port for webhook"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+  tags = local.common_tags
+}
+
+############### EC2 json-filter ############
+module "ec2-instance-service-json" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 3.0"
+  create                 = var.ec2_instances_create[local.env_name]
+  name                   = "json_filter_${local.env_name}_${var.env_class}.${var.route_53_private_zone_name[local.env_name]}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = "deploy"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-070712bd20c3ac748", "sg-00aebda5b39acaef6", module.security-group-json.security_group_id]
+  subnet_id              = var.subnet_id[local.env_name]
+  tags = merge(
+    {
+      group = "json_filter"
+    },
+    local.common_tags
+  )
+}
+
+######## Route53 / Target groups / Loadbalancers ###########
+resource "aws_lb_target_group_attachment" "frontend" {
+  count            = var.ec2_instances_create[local.env_name] ? 1 : 0
+  target_group_arn = var.target_group_arn
+  target_id        = module.ec2-instance-service["frontend"].id
+  port             = 5000
 }
--- sandbox/euc101/variables.tf
+++ sandbox/euc101/variables.tf
@@ -10,12 +10,84 @@ variable "env_class" {
   default     = "sandbox"
 }
 
+#################################################
 variable "vpc_id" {
   type        = map(string)
-  description = "vpc-0d14e4956bccdc439" # default VPC
+  description = "Envariroment VPC"
 }
 
+variable "subnet_id" {
+  type        = map(string)
+  description = "Default Subnet"
+}
+
+variable "ami" {
+  type        = string
+  description = "Default AMI"
+  default     = "ami-09439f09c55136ecf"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Default instance type"
+  default     = "t3a.nano"
+}
+
+variable "route_53_private_zone_name" {
+  type        = map(string)
+  description = "Envarioment Route 53 zone"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  default     = ["subnet-0ad013438ee134ad6"]
+  description = "Default Subnet"
+}
+
+variable "target_group_arn" {
+  type        = string
+  description = "ui target group"
+  default     = "arn:aws:elasticloadbalancing:eu-central-1:779414916509:targetgroup/ui/16f43c5cda7c19d6"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "Security group ids"
+  default     = ["sg-070712bd20c3ac748", "sg-00aebda5b39acaef6"]
+}
+
+####################################################3
 variable "rabbitmq_create" {
   type        = map(bool)
   description = "Whether to create rabbitmq resources or not"
+}
+
+variable "rds_create" {
+  type        = map(bool)
+  description = "Whether to create rds resources or not"
+}
+
+variable "ec2_instances_create" {
+  type        = map(bool)
+  description = "Whether to create EC2s resources or not"
+}
+
+############ users & passwords ###########
+variable "dbuser" {
+  type    = string
+  default = "dbuser"
+}
+
+variable "dbpass" {
+  type        = string
+  description = "Password for user of DB"
+}
+
+variable "mquser" {
+  type    = string
+  default = "mquser"
+}
+variable "mqpass" {
+  type        = string
+  description = "Password for user of MQ"
 }
